cbd00df860ef283f8637d524de0b7434ccfcfcfc
DasRaseswari <das.r@husky.neu.edu>
2020-05-24 14:46:00 -0400

    setting up MySQL db and setting up db connection

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error.js');
 
+const db = require('./util/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
